ce7af90d34b673035b78df5f115b29937cf6676c
Kieran Meinhardt <kmein@posteo.de>
2026-05-27 16:14:22 +0200

    Add bye job

--- release.nix
+++ release.nix
@@ -11,4 +11,12 @@ let
 in
 {
   hello = pkgs.hello;
+  bye = pkgs.hello.overrideAttrs (oldAttrs: {
+    pname = "bye";
+    doCheck = false;
+    postPatch = (oldAttrs.postPatch or "") + ''
+      substituteInPlace src/hello.c \
+        --replace-fail 'Hello, world!' 'Bye, world!'
+    '';
+  });
 }
